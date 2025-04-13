fa18444a5b0c06dcf1cbb9914ce34676642d1354
ursinhogit <vvaniaoliveira@gmail.com>
2025-04-13 20:38:12 +0000

    desafio_xadrex

---
+++ desafio_xadrez.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main (){
+    char torre, bispo, rainha;
+    int i,j;
+    
+    printf("digite pule 5 casas para a direita :torre\n");
+    scanf("%c",&torre);
+
+    printf("digite pule 5  casas acima: bispo\n");
+    scanf("% c", &torre);
+
+    printf ("digite pulo livre : rainha\n ");
+    scanf("%c",&rainha);
+
+      for( int i=0; j=4; i<=j;i++){
+      for (int i=0; j=4; i<=j;i++){
+
+    do
+    {int j;
+    } while (i<j=4; i++){
+    }
+    int i=0;j=4;i< j= i++  
+   }
+        printf("\n");
+    
+return 0;
+      
+}
